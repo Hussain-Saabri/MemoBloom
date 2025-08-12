ee828d13b72fc9f446a9e8f8ace10724c9eff9bf
Hussain-Saabri <dadapir19ce30@gmail.com>
2025-08-12 22:27:08 +0530

    upadate base_url with deployed link

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:8000/' || 'https://memobloom-backend-1.onrender.com/';
-
+export const BASE_URL = process.env.NODE_ENV === 'development' 
+  ? 'http://localhost:8000/' 
+  : 'https://memobloom-backend-1.onrender.com/';
